18f0daa39633a9b41a4bbc798333450eac2eec60
Daniel Hart <86760228+dani3ljh@users.noreply.github.com>
2024-01-15 23:36:57 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# elementary automata
+inspired by [this video](https://www.youtube.com/watch?v=Ggxt06qSAe4)
+still wip and still learning rust
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+const CELL_LENGTH: usize = 30;
+const LOOP_AMOUNT: u32 = 30;
+
+fn main() {
+    const RULESET: u8 = 0b01001101;
+    const ALIVE_CHAR: char = '█'; // full block
+    const DEAD_CHAR: char = ' ';
+    const WALL_CHAR: char = '|';
+
+    let mut cells = [false; CELL_LENGTH]; 
+    cells[CELL_LENGTH / 2] = true;
+
+    while i in 0..LOOP_AMOUNT {
+        print_cells(&cells, &ALIVE_CHAR, &DEAD_CHAR, &WALL_CHAR);
+        calculate_new_cells(&mut cells, &RULESET);
+    }
+}
+
+fn print_cells(cells: &[bool; CELL_LENGTH], alive_char: &char, dead_char: &char, wall_char: &char) {
+    print!("{wall_char}");
+    for cell in cells {
+        print!("{}", if *cell {alive_char} else {dead_char});
+    }
+    println!("{wall_char}");
+}
+
+fn calculate_new_cells(cells: &mut [bool;CELL_LENGTH], ruleset: &u8) {
+    // TODO: finish this
+}
